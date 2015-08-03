74392f3039a24bf74c6f6b4f59adbe6dd884d69e
Alexis Montagne <alexis.montagne@gmail.com>
2015-08-03 11:04:14 -0700

    Add the sensu-coreos script to add machine metadatas to etcd

--- scripts/host_cleanup.sh
+++ scripts/host_cleanup.sh
@@ -6,4 +6,9 @@ docker rm $(docker ps -q -f status=exited)
 # attempt to clean all unused images
 docker rmi $(docker images -a -q)
 
+curl -sL https://raw.githubusercontent.com/upfluence/sensu-coreos/master/clients/sensu-fleet-client/export_infos.sh \
+  > export_infos.sh
+
+sh export_infos.sh
+
 exit 0
