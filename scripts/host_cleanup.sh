a601a0a7a0a1da40b04847d58353820cdf26230d
Alexis Montagne <alexis.montagne@gmail.com>
2015-08-25 10:22:46 -0700

    Disable the docker rmi command

--- scripts/host_cleanup.sh
+++ scripts/host_cleanup.sh
@@ -4,7 +4,7 @@
 docker rm $(docker ps -q -f status=exited)
 
 # attempt to clean all unused images
-docker rmi $(docker images -a -q)
+# docker rmi $(docker images -a -q)
 
 curl -sL https://raw.githubusercontent.com/upfluence/sensu-coreos/master/clients/sensu-fleet-client/export_infos.sh \
   > export_infos.sh
