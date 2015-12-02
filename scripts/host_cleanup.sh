ae92b757b4bfb417d2294725de828fa0b022c586
Alexis Montagne <alexis.montagne@gmail.com>
2015-12-02 15:46:34 -0800

    Flush dangling docker images

--- scripts/host_cleanup.sh
+++ scripts/host_cleanup.sh
@@ -4,7 +4,7 @@
 docker rm $(docker ps -q -f status=exited)
 
 # attempt to clean all unused images
-# docker rmi $(docker images -a -q)
+docker images -f dangling=true -q | xargs docker rmi
 
 curl -sL https://raw.githubusercontent.com/upfluence/sensu-coreos/master/clients/sensu-fleet-client/export_infos.sh \
   > export_infos.sh
